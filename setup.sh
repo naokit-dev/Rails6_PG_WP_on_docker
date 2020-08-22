ed7a9de76c40daa5402b82f8bdd40b1955e79e10
naokit-dev <takeshita.na@gmail.com>
2020-08-22 22:27:37 +0900

    add css config

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "docker-compose run app rails new . --force --no-deps --database=postgresql --skip-bundle"
 docker-compose run app rails new . --force --no-deps --database=postgresql --skip-bundle
 
@@ -8,14 +10,22 @@ echo "docker-compose run app rails webpacker:install"
 docker-compose run app rails webpacker:install
 
 echo "set check_yarn_integrity: false"
-sed -ie 's/check_yarn_integrity: true/check_yarn_integrity: false/g' config/webpacker.yml
-# echo "docker-compose run app yarn add webpack-dev-server"
-# docker-compose run app yarn add webpack-dev-server
+sed -icp 's/check_yarn_integrity: true/check_yarn_integrity: false/g' config/webpacker.yml
 
 echo "copy config files"
-mv setup_files/copy_database.yml config/database.yml 
-rmdir setup_files
+mv temp_files/copy_database.yml config/database.yml
 
 echo "docker-compose run app rake db:create"
 docker-compose run app rake db:create
+
+echo "create CSS for Webpack"
+mkdir app/javascript/stylesheets
+touch app/javascript/stylesheets/application.scss
+mv temp_files/copy_application.html.erb 
+
+echo "clean temp filse"
+rm -r temp_files
+rm config/webpacker.ymlcp
+
+
 
