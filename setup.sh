d6854d682fe6978d80660f01dcb3477c4568e7f0
naokit-dev <takeshita.na@gmail.com>
2020-08-24 16:22:11 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -7,33 +7,33 @@ install_Bootstrap=true
 install_FontAwesome=true
 ###################
 
-# echo "docker-compose run app rails new . --force --no-deps --database=postgresql --skip-bundle"
-# docker-compose run app rails new . --force --no-deps --database=postgresql --skip-bundle
+echo "docker-compose run app rails new . --force --no-deps --database=postgresql --skip-bundle"
+docker-compose run app rails new . --force --no-deps --database=postgresql --skip-bundle
 
-# echo "docker-compose build"
-# docker-compose build
+echo "docker-compose build"
+docker-compose build
 
-# echo "docker-compose run app rails webpacker:install"
-# docker-compose run app rails webpacker:install
+echo "docker-compose run app rails webpacker:install"
+docker-compose run app rails webpacker:install
 
-# echo "set check_yarn_integrity: false"
-# sed -icp 's/check_yarn_integrity: true/check_yarn_integrity: false/g' config/webpacker.yml
+echo "set check_yarn_integrity: false"
+sed -icp 's/check_yarn_integrity: true/check_yarn_integrity: false/g' config/webpacker.yml
 
-# echo "copy config files"
-# mv temp_files/copy_database.yml config/database.yml
+echo "copy config files"
+mv temp_files/copy_database.yml config/database.yml
 
-# echo "docker-compose run app rake db:create"
-# docker-compose run app rake db:create
+echo "docker-compose run app rake db:create"
+docker-compose run app rake db:create
 
-# echo "create CSS for Webpack"
-# mkdir app/javascript/stylesheets
-# touch app/javascript/stylesheets/application.scss
-# mv temp_files/copy_application.html.erb app/views/layouts/application.html.erb
-# echo 'import "../stylesheets/application.scss";' >> app/javascript/packs/application.js
+echo "create CSS for Webpack"
+mkdir app/javascript/stylesheets
+touch app/javascript/stylesheets/application.scss
+mv temp_files/copy_application.html.erb app/views/layouts/application.html.erb
+echo 'import "../stylesheets/application.scss";' >> app/javascript/packs/application.js
 
 
 if "$opptinal_packages" ; then
-if "$install_FontAwesome" ; then
+if "$install_Bootstrap" ; then
 echo "install Bootstrap"
 docker-compose run app yarn add bootstrap jquery popper.js --ignore-optional
 echo 'require("bootstrap");' >> app/javascript/packs/application.js
